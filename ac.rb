17e73ee0af6a3f1779d29d0046e8800015820807
Brian C. McFarlane <me@briancmcfarlane.com>
2013-01-02 12:00:28 -0500

    Moved all database requests into the post request

--- ac.rb
+++ ac.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 require 'sqlite3'
 
-db = SQLite3::Database.new( 'ac.db' )
-
 get '/' do
   erb :home
 end
 
 post '/' do
-  params[:query]
+  @db = SQLite3::Database.new('ac.db')
+  @lookup = @db.execute("select price from item where name = ?", params[:query]).join("")
+  if @lookup != ''
+    @result = @lookup
+  else
+    @result = 'No item by that name.'
+  end
+  erb :home
 end
