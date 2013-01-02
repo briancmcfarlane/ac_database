353f0c033b9cabbb3ef7a7514b0edcc019368fb0
Brian C. McFarlane <me@briancmcfarlane.com>
2013-01-01 21:26:10 -0500

    Added databasemapper

--- ac.rb
+++ ac.rb
@@ -1,4 +1,14 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/ac.db")
+class Item
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, Text
+  property :price, Integer
+  property :type, Text
+end
 
 get '/' do
   erb :home
