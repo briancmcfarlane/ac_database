56a81d21a3bf37a5e6825899c540f695982f23fe
Brian C. McFarlane <me@briancmcfarlane.com>
2013-01-01 21:37:20 -0500

    Adjust formatting

--- ac.rb
+++ ac.rb
@@ -2,14 +2,20 @@ require 'sinatra'
 require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/ac.db")
+
 class Item
   include DataMapper::Resource
-  property :id, Serial
-  property :name, Text
-  property :price, Integer
-  property :type, Text
+
+  property :id,     Serial
+  property :name,   Text
+  property :price,  Integer
+  property :type,   Text
 end
 
 get '/' do
   erb :home
+end
+
+post '/' do
+  params[:query]
 end
