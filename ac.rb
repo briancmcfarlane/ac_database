093c66565acba95f0227b92543398f6a1fe29c7d
Brian C. McFarlane <me@briancmcfarlane.com>
2013-01-02 12:15:32 -0500

    Appended 'bells' to price of item returned

--- ac.rb
+++ ac.rb
@@ -9,7 +9,7 @@ post '/' do
   @db = SQLite3::Database.new('ac.db')
   @lookup = @db.execute("select price from item where name = ?", params[:query]).join("")
   if @lookup != ''
-    @result = @lookup
+    @result = @lookup + ' bells'
   else
     @result = 'No item by that name.'
   end
